7732f743cff8a3164475a542a940d543edd88fa1
sunnyh404 <yiwen_90@hotmail.com>
2023-03-06 17:33:17 -0500

    Remove unused variable

--- src/main/java/ca/concordia/soen/ThrowsDestructiveWrappingVisitor.java
+++ src/main/java/ca/concordia/soen/ThrowsDestructiveWrappingVisitor.java
@@ -10,41 +10,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ThrowsDestructiveWrappingVisitor extends ASTVisitor {
-    static int exceptionsThreshold = 3;
+	static int exceptionsThreshold = 3;
 
-    private final CompilationUnit compilationUnit;
-    int AntiPatternOccurrencesCount = 0;
-    List<AntiPatternOccurrence> ThrowsDestructiveWrappingOcurrencesList = new ArrayList<>();
+	private final CompilationUnit compilationUnit;
+	int AntiPatternOccurrencesCount = 0;
+	List<AntiPatternOccurrence> ThrowsDestructiveWrappingOcurrencesList = new ArrayList<>();
 
-    public ThrowsDestructiveWrappingVisitor(CompilationUnit compilationUnit) {
-        this.compilationUnit = compilationUnit;
-    }
+	public ThrowsDestructiveWrappingVisitor(CompilationUnit compilationUnit) {
+		this.compilationUnit = compilationUnit;
+	}
 
-    @Override
+	@Override
 	public boolean visit(MethodDeclaration method) {
 		if (method.getBody() != null) {
 
 			method.getBody().accept(new ASTVisitor() {
-				
+
 				@Override
 				public boolean visit(CatchClause clause) {
 
 					if (clause.getBody().statements().isEmpty() == false) {
-						
-						
+
+
 						clause.getBody().accept(new ASTVisitor() {
 
 							public boolean visit(ThrowStatement node) {
-					            AntiPatternOccurrencesCount += 1;
-					            int startLine = compilationUnit.getLineNumber(method.getStartPosition());
-					            String functionName = method.getName().toString();
-					            AntiPatternOccurrence ThrowsDestructiveWrappingOccurrence = new AntiPatternOccurrence(functionName, Integer.toString(startLine));
-					            ThrowsDestructiveWrappingOcurrencesList.add(ThrowsDestructiveWrappingOccurrence);
-						        return super.visit(node);
-						    }
-						
-							
-					});
+								AntiPatternOccurrencesCount += 1;
+								int startLine = compilationUnit.getLineNumber(method.getStartPosition());
+								String functionName = method.getName().toString();
+								AntiPatternOccurrence ThrowsDestructiveWrappingOccurrence = new AntiPatternOccurrence(functionName, Integer.toString(startLine));
+								ThrowsDestructiveWrappingOcurrencesList.add(ThrowsDestructiveWrappingOccurrence);
+								return super.visit(node);
+							}
+
+
+						});
 
 					}
 
@@ -57,5 +57,5 @@ public class ThrowsDestructiveWrappingVisitor extends ASTVisitor {
 
 		return true;
 	}
-    
+
 }
