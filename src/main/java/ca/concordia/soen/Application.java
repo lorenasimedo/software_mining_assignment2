8b8de9ccad6e1ce532fd96bd6f10e4295f04f9a6
Lorena Barreto <lorenasimedo@gmail.com>
2023-03-07 18:38:17 -0500

    Fixing the problem to generate the output file that was happening on Windows

--- src/main/java/ca/concordia/soen/Application.java
+++ src/main/java/ca/concordia/soen/Application.java
@@ -1,18 +1,15 @@
 package ca.concordia.soen;
 
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
 import com.google.gson.JsonObject;
 
 import java.util.List;
 
 public class Application {
 
-    static String output_path = "../../../output/";
+    static String output_folder = "output";
+
 
     public static void main(String[] args) {
-
-        ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
 
         String folderName = args[0];
         String[] path = folderName.split("/");
@@ -24,6 +21,7 @@ public class Application {
         JsonObject ThrowsKitchenSinkJson = ThrowsKitchenSinkFinder.getThrowsKitchenSinkOccurrences(allFiles);
         //TODO: Add the other antipatterns here
         AntiPatternsJson.add("ThrowsKitchenSink", ThrowsKitchenSinkJson);
-        FileUtil.writeJsonFile(AntiPatternsJson, "output",  projectName);
+
+        FileUtil.writeJsonFile(AntiPatternsJson, output_folder,  projectName);
     }
 }
