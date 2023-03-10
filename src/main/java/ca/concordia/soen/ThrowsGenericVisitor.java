5fce3154f32b5db457b9e5c3b6a36d1e10e5ec48
nakhlarafi <nakhla054@gmail.com>
2023-03-10 18:40:39 -0500

    Feature changes based on the comments from Lorena

--- src/main/java/ca/concordia/soen/ThrowsGenericVisitor.java
+++ src/main/java/ca/concordia/soen/ThrowsGenericVisitor.java
@@ -18,128 +18,34 @@ public class ThrowsGenericVisitor  extends ASTVisitor {
     public boolean visit(MethodDeclaration node) {
         List<Type> thrownExceptions = node.thrownExceptionTypes();
         CompilationUnit cu = compilationUnit;
-        boolean throwInTry = false;
-        boolean throwInCatch = false;
-        boolean throwDetected = false;
-        boolean genericExceptionInCatch = false;
-        boolean containsOnlyExceptionInTry = false;
         Block body = node.getBody();
         if(thrownExceptions.size() == 1){
-            for (Type thrownException : thrownExceptions) {
-                if (thrownException instanceof SimpleType && thrownException.toString().equals("Exception")) {
-                    if (hasTryCatch(body)){
-                        for (Statement statement : (List<Statement>) body.statements()) {
-                            if (statement.getNodeType() == ASTNode.TRY_STATEMENT) {
-                                TryStatement tryStatement = (TryStatement) statement;
-                                List<Statement> tryStatementsInBody = tryStatement.getBody().statements();
-                                containsOnlyExceptionInTry = containsOnlyExceptionThrowStatements(tryStatementsInBody);
-                                List<CatchClause> catchClauses = tryStatement.catchClauses();
-                                for (CatchClause catchClause: catchClauses){
-                                    if (!catchClause.getException().getType().toString().equals("Exception")){
-                                        genericExceptionInCatch = false;
-                                    }
-                                    else {
-                                        genericExceptionInCatch = true;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-                        if(genericExceptionInCatch && containsOnlyExceptionInTry){
-                            antiPatternDetect(node, cu, thrownException.getStartPosition());
-                        }
-                    }
-                    else {
-                        if (body.statements().isEmpty()) throwDetected = true;
-                        for (Statement statement : (List<Statement>) body.statements()) {
-                            if(statement.getNodeType() == ASTNode.THROW_STATEMENT){
-                                Expression expression = ((ThrowStatement) statement).getExpression();
-                                if (expression.getNodeType() == ASTNode.CLASS_INSTANCE_CREATION){
-                                    ClassInstanceCreation cic = (ClassInstanceCreation) expression;
-                                    if (cic.getType().toString().equals("Exception")){
-                                        throwDetected = true;
-                                    }
-                                    break;
-                                }
-                            }
-                            else {
-                                throwDetected = true;
-                            }
-                        }
-                        if (throwDetected){
-                            antiPatternDetect(node, cu, thrownException.getStartPosition());
-                        }
-                    }
-                }
+            if (thrownExceptions.get(0) instanceof SimpleType && thrownExceptions.get(0).toString().equals("Exception")) {
+                antiPatternDetect(node, cu, thrownExceptions.get(0).getStartPosition());
             }
         }
         else if (thrownExceptions.size() == 0){
-            Statement s = null;
-            int throwCount = 0;
-            if(body != null){
-                for (Statement statement : (List<Statement>) body.statements()) {
+            if(hasTryCatch(body)){
+                for(Statement statement: (List<Statement>) body.statements()){
                     if (statement.getNodeType() == ASTNode.TRY_STATEMENT) {
                         TryStatement tryStatement = (TryStatement) statement;
-                        Block tryBody = tryStatement.getBody();
-                        List<Statement> tryStatementsInBody = tryBody.statements();
-                        if (tryStatementsInBody.isEmpty()) {
-                            throwInTry = true;
-                            s = statement;
-                        }
-                        for (Statement tryStatementInBody : tryStatementsInBody){
-                            if (tryStatementInBody.getNodeType() == ASTNode.THROW_STATEMENT){
-                                throwCount += 1;
-                                Expression expression = ((ThrowStatement) tryStatementInBody).getExpression();
-                                if (expression.getNodeType() == ASTNode.CLASS_INSTANCE_CREATION){
-                                    ClassInstanceCreation cic = (ClassInstanceCreation) expression;
-                                    if (cic.getType().toString().equals("Exception")){
-                                        throwInTry = true;
-                                        s = tryStatementInBody;
-                                    }
-                                    else {
-                                        throwInTry = false;
-                                    }
-                                    break;
-                                }
-                            }
-                            else {
-                                throwInTry = true;
-                            }
-                        }
-
                         List<CatchClause> catchClauses = tryStatement.catchClauses();
-                        if (catchClauses.size() == 1 && catchClauses.get(0).getException().getType().toString().equals("Exception")){
-                            Block catchBody = catchClauses.get(0).getBody();
-                            List<Statement> catchStatementsInBody = catchBody.statements();
-                            if (catchStatementsInBody.size() == 0){
-                                throwInCatch = true;
-                            }
-                            else {
-                                for(Statement catchStatementInBody : catchStatementsInBody){
-                                    if (catchStatementInBody.getNodeType() == ASTNode.THROW_STATEMENT){
-                                        throwCount += 1;
-                                        Expression expression = ((ThrowStatement) catchStatementInBody).getExpression();
-                                        if (expression.getNodeType() == ASTNode.CLASS_INSTANCE_CREATION){
-                                            ClassInstanceCreation cic = (ClassInstanceCreation) expression;
-                                            if (cic.getType().toString().equals("Exception")){
-                                                throwInCatch = true;
+                        for (CatchClause catchClause : catchClauses){
+                            SimpleName exceptionName = catchClause.getException().getName();
+                            if (catchClause.getException().getType().toString().equals("Exception")){
+                                List<Statement> catchStatements = catchClause.getBody().statements();
+                                if (catchStatements!=null){
+                                    for (Statement catchStatement : catchStatements){
+                                        if (catchStatement instanceof ThrowStatement) {
+                                            Expression exception = ((ThrowStatement) catchStatement).getExpression();
+                                            if (exception instanceof SimpleName && ((SimpleName) exception).getIdentifier().equals(exceptionName.getIdentifier())) {
+                                                antiPatternDetect(node, cu, catchStatement.getStartPosition());
                                             }
-                                            else {
-                                                throwInCatch = false;
-                                            }
-                                            break;
                                         }
                                     }
-                                    else {
-                                        throwInCatch = true;
-                                    }
                                 }
                             }
                         }
-                    }
-
-                    if (throwInCatch && throwInTry && throwCount>0){
-                        antiPatternDetect(node, cu, s.getStartPosition());
                     }
                 }
             }
@@ -161,25 +67,6 @@ public class ThrowsGenericVisitor  extends ASTVisitor {
             }
         }
         return false;
-    }
-    public boolean containsOnlyExceptionThrowStatements(List<Statement> bodyStatements) {
-        boolean foundException = true;
-        //System.out.println(bodyStatements);
-        for (Statement statement : bodyStatements) {
-            System.out.println(statement.getNodeType());
-            if(statement.getNodeType() == ASTNode.THROW_STATEMENT){
-                Expression expression = ((ThrowStatement) statement).getExpression();
-                if (expression.getNodeType() == ASTNode.CLASS_INSTANCE_CREATION){
-                    ClassInstanceCreation cic = (ClassInstanceCreation) expression;
-                    if (!cic.getType().toString().equals("Exception")) {
-                        foundException = false;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return foundException;
     }
 
 }
