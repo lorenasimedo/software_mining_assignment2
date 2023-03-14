a9648b7d6e4c474fa447d5e9abf416d90f14c5c8
sunnyh404 <yiwen_90@hotmail.com>
2023-03-14 13:09:51 -0400

    Undo accidental file writing in previous commit

--- src/main/java/ca/concordia/soen/Application.java
+++ src/main/java/ca/concordia/soen/Application.java
@@ -27,6 +27,7 @@ public class Application {
         JsonObject ThrowsDestructiveWrappingJson = ThrowsDestructiveWrappingFinder.getThrowsDestructiveWrappingOccurrences(allFiles);
         //TODO: Add the other antipatterns here
         AntiPatternsJson.add("ThrowsDestructiveWrapping", ThrowsDestructiveWrappingJson);
-        
+    	
+        FileUtil.writeJsonFile(AntiPatternsJson, output_folder,  projectName);
     }
 }
