e43c434c5b5c1b3a9877d24012c1a28fe0ac9406
sunnyh404 <yiwen_90@hotmail.com>
2023-03-14 12:57:40 -0400

    Remove redundant file writing after detection.

--- src/main/java/ca/concordia/soen/Application.java
+++ src/main/java/ca/concordia/soen/Application.java
@@ -1,21 +1,20 @@
 package ca.concordia.soen;
 
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
 import com.google.gson.JsonObject;
 
+import java.io.File;
 import java.util.List;
 
 public class Application {
 
-    static String output_path = "../../../output/";
+    static String output_folder = "output";
+
 
     public static void main(String[] args) {
 
-        ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
-
         String folderName = args[0];
-        String[] path = folderName.split("/");
+        String splitter = File.separator.replace("\\","\\\\");
+        String[] path = folderName.split(splitter);
         String projectName = path[path.length - 1];
 
         List<String> allFiles = FileUtil.getAllFileNames(folderName);
@@ -24,13 +23,10 @@ public class Application {
         JsonObject ThrowsKitchenSinkJson = ThrowsKitchenSinkFinder.getThrowsKitchenSinkOccurrences(allFiles);
         //TODO: Add the other antipatterns here
         AntiPatternsJson.add("ThrowsKitchenSink", ThrowsKitchenSinkJson);
-        FileUtil.writeJsonFile(AntiPatternsJson, "output",  projectName);
-        
 
         JsonObject ThrowsDestructiveWrappingJson = ThrowsDestructiveWrappingFinder.getThrowsDestructiveWrappingOccurrences(allFiles);
         //TODO: Add the other antipatterns here
         AntiPatternsJson.add("ThrowsDestructiveWrapping", ThrowsDestructiveWrappingJson);
-        FileUtil.writeJsonFile(AntiPatternsJson, "output",  projectName);
         
     }
 }
