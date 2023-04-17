497345892640fe66e2c79e38963d8bb056026be0
nakhlarafi <nakhla054@gmail.com>
2023-04-17 17:12:31 -0400

    new ASTVisitor added for checking throw statements in the body

--- src/main/java/ca/concordia/soen/ThrowsGenericVisitor.java
+++ src/main/java/ca/concordia/soen/ThrowsGenericVisitor.java
@@ -5,6 +5,7 @@ import org.eclipse.jdt.core.dom.*;
 import java.util.List;
 
 public class ThrowsGenericVisitor extends AntiPatternVisitor {
+
     public ThrowsGenericVisitor(CompilationUnit compilationUnit) {
         super(compilationUnit);
     }
@@ -13,17 +14,15 @@ public class ThrowsGenericVisitor extends AntiPatternVisitor {
     public boolean visit(MethodDeclaration node) {
         List<Type> methodDeclarationThrownExceptions = node.thrownExceptionTypes();
         Block body = node.getBody();
-        if(methodDeclarationThrownExceptions.size() == 1){
+        if (methodDeclarationThrownExceptions.size() == 1) {
             if (methodDeclarationThrownExceptions.get(0) instanceof SimpleType && methodDeclarationThrownExceptions.get(0).toString().equals("Exception")) {
                 antiPatternDetect(node, methodDeclarationThrownExceptions.get(0).getStartPosition());
             }
-        }
-        else if (methodDeclarationThrownExceptions.size() == 0){
-            if(hasTryCatch(body)){
-                for(Object statement: body.statements()){
+        } else if (methodDeclarationThrownExceptions.size() == 0) {
+            if (hasTryCatch(body)) {
+                for (Object statement : body.statements()) {
                     if (statement instanceof TryStatement tryStatement) {
-                        List<CatchClause> catchClauses;
-                        catchClauses = tryStatement.catchClauses();
+                        List<CatchClause> catchClauses = tryStatement.catchClauses();
                         checkCatchClausesForThrowsGeneric(catchClauses, node);
                     }
                 }
@@ -31,53 +30,34 @@ public class ThrowsGenericVisitor extends AntiPatternVisitor {
         }
         return true;
     }
+
     private void checkCatchClausesForThrowsGeneric(List<CatchClause> catchClauses, MethodDeclaration node) {
         for (CatchClause catchClause : catchClauses) {
             if ("Exception".equals(catchClause.getException().getType().toString())) {
-                List<Statement> catchStatements = catchClause.getBody().statements();
-                processCatchStatements(catchStatements, catchClause.getException().getName(), node);
+                SimpleName exceptionName = catchClause.getException().getName();
+                catchClause.accept(new ASTVisitor() {
+                    @Override
+                    public boolean visit(ThrowStatement catchStatement) {
+                        Expression exception = catchStatement.getExpression();
+                        if (exception instanceof SimpleName && ((SimpleName) exception).getIdentifier().equals(exceptionName.getIdentifier())) {
+                            antiPatternDetect(node, catchStatement.getStartPosition());
+                        }
+                        return super.visit(catchStatement);
+                    }
+                });
             }
         }
     }
 
-    public void processCatchStatements(List<Statement> catchStatements, SimpleName exceptionName, MethodDeclaration node) {
-        try {
-            for (Statement catchStatement : catchStatements) {
-                processStatement(catchStatement, exceptionName, node);
-            }
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void processStatement(Statement statement, SimpleName exceptionName, MethodDeclaration node) {
-        if (statement instanceof ThrowStatement) {
-            Expression exception = ((ThrowStatement) statement).getExpression();
-            if (exception instanceof SimpleName && ((SimpleName) exception).getIdentifier().equals(exceptionName.getIdentifier())) {
-                antiPatternDetect(node, statement.getStartPosition());
-            }
-        } else if (statement instanceof IfStatement) {
-            processStatement(((IfStatement) statement).getThenStatement(), exceptionName, node);
-            Statement elseStatement = ((IfStatement) statement).getElseStatement();
-            if (elseStatement != null) {
-                processStatement(elseStatement, exceptionName, node);
-            }
-        } else if (statement instanceof Block) {
-            List<Statement> blockStatements = ((Block) statement).statements();
-            for (Statement blockStatement : blockStatements) {
-                processStatement(blockStatement, exceptionName, node);
-            }
-        }
-    }
     private void antiPatternDetect(MethodDeclaration node, int startPosition) {
         int startLine = compilationUnit.getLineNumber(startPosition);
-        antiPatternOccurrencesCount +=1;
+        antiPatternOccurrencesCount += 1;
         AntiPatternOccurrence ThrowsGenericOccurrence = new AntiPatternOccurrence(node.getName().toString(), Integer.toString(startLine));
         antiPatternOcurrencesList.add(ThrowsGenericOccurrence);
     }
 
     public static boolean hasTryCatch(Block block) {
-        if (block != null){
+        if (block != null) {
             for (Object statement : block.statements()) {
                 if (statement instanceof TryStatement) {
                     return true;
